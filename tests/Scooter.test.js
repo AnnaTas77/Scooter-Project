d6c6f89c229146002929d3cc866763841703be13
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-26 12:41:51 +0100

    Created the Scooter and User class and added test suites (followed TDD). Added UML class diagrams.

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,23 +1,63 @@
-const Scooter = require('../src/Scooter')
+const { describe, expect, test, it } = require("@jest/globals");
+const Scooter = require("../src/Scooter");
+const User = require("../src/User");
 
 // typeof scooter === object
-describe('scooter object', () => {
-  test('Scooter class should create Scooter instance', () => {
-    const scooter = new Scooter()
-    expect(scooter).toBeInstanceOf(Scooter)
-  })
-})
+describe("Scooter class", () => {
+  const scooter = new Scooter("Battersea Power Station");
+  test("Scooter class should create Scooter instance", () => {
+    expect(scooter).toBeInstanceOf(Scooter);
+  });
+  test("Scooter class should have a station property, which is a string", () => {
+    expect(scooter.station).toBe("Battersea Power Station");
+  });
+  test("Scooter class should have a serial property, which is a number", () => {
+    expect(typeof scooter.serial).toBe("number");
+  });
+  test("Scooter class should have a charge property, which is a number", () => {
+    expect(typeof scooter.charge).toBe("number");
+  });
+  test("Scooter class should have a isBroken property, which is a boolean", () => {
+    expect(typeof scooter.isBroken).toBe("boolean");
+  });
+});
 
 // Method tests
-describe('scooter methods', () => {
-  // tests here!
+describe("Scooter class methods", () => {
+  it("accepts user instance of the User class as an argument", () => {
+    const scooter = new Scooter("Battersea Power Station");
+    const user1 = new User("sam123", 1234, 25);
+    scooter.rent(user1);
+    expect(scooter.user).toEqual(user1);
+  });
 
-  // rent method
+  it("assigns a scooter to a user if it is charged and not broken", () => {
+    const scooter = new Scooter("Battersea Power Station");
+    const user1 = new User("sam123", 1234, 25);
 
+    scooter.rent(user1);
+
+    expect(scooter.station).toBeNull();
+    expect(scooter.user).toBe(user1);
+  });
+
+  it("throws an error if the scooter is not charged", () => {
+    const scooter = new Scooter("Battersea Power Station");
+    const user1 = new User("sam123", 1234, 25);
+
+    scooter.charge = 10;
+
+    function attemptRental() {
+      scooter.rent(user1);
+    }
+
+    expect(attemptRental).toThrow();
+    expect(scooter.station).toBe("Battersea Power Station");
+    expect(user1.loggedIn).toBe(false);
+    expect(scooter.user).toBeNull();
+  });
+  test("should first", () => {});
   // dock method
-
   // requestRepair method
-
   // charge method
-
-})
+});
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,18 +1,45 @@
-const User = require('../src/User')
-
-const user = new User('Joe Bloggs', 'test123', 21)
+const { describe, expect, test, it } = require("@jest/globals");
+const User = require("../src/User");
 
 // User tests here
-describe('User property tests', () => {
+describe("User property tests", () => {
   // test username
-  test('username should be a string', () => {
-    expect(typeof user.username).toBe('string')
-  })
+  test("the username should be a string", () => {
+    const user = new User("Joe Bloggs", "test123", 21);
+    expect(typeof user.username).toBe("string");
+  });
   // test password
+  test("the password should be a string", () => {
+    const user = new User("Joe Bloggs", "test123", 21);
+    expect(typeof user.username).toBe("string");
+  });
 
   // test age
-})
+  test("username should be a string", () => {
+    const user = new User("Joe Bloggs", "test123", 21);
+    expect(typeof user.username).toBe("string");
+  });
+});
 
 // test login
+describe("Testing User Login Method", () => {
+  test("should login the user when they provide a correct password", () => {
+    const user = new User("Joe Bloggs", "test123", 21);
+
+    user.login("test123");
+
+    expect(user.loggedIn).toBe(true);
+  });
+
+  test("throws an error if the password is incorrect", () => {
+    const user = new User("Joe Bloggs", "password", 21);
+
+    function tryLogin() {
+      user.login("wrongpass");
+    }
+
+    expect(tryLogin).toThrow("Incorrect password.");
+  });
+});
 
 // test logout
