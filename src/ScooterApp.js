2e67d4d1e9f403e60e2f88d6f7613afdccb266ba
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-26 15:01:33 +0100

    Added and tested the registerUser() method of the ScooterApp class

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -1,7 +1,46 @@
-// require the User and Scooter classes - see where they can be used in ScooterApp.js
+const Scooter = require("./Scooter");
+const User = require("./User");
 
 class ScooterApp {
   // ScooterApp code here
+  stations = {
+    "Battersea Power Station": [],
+    "London Bridge": [],
+    Victoria: [],
+    Piccadilly: [],
+    Richmond: [],
+  };
+
+  registeredUsers = {};
+
+  registerUser(username, password, age) {
+    const newUser = new User(username, password, age);
+    const currentUserName = newUser.username;
+
+    if (this.registeredUsers.hasOwnProperty(currentUserName)) {
+      throw new Error("This user is already registered.");
+    }
+
+    if (newUser.age < 18) {
+      throw new Error("This user is too young to register.");
+    }
+
+    if (
+      !(this.registeredUsers.hasOwnProperty(currentUserName)) &&
+      newUser.age >= 18
+    ) {
+      this.registeredUsers[currentUserName] = newUser;
+    }
+    console.log("A new user has been registered.");
+    return newUser;
+  }
+
+  loginUser(username, password) {}
+  logoutUser(username) {}
+  createScooter(station) {}
+  dockScooter(scooter, station) {}
+  rentScooter(scooter, user) {}
+  print() {}
 }
 
-module.exports = ScooterApp
+module.exports = ScooterApp;
--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -1,16 +1,38 @@
-// const User = require('../src/User')
-// const ScooterApp = require('../src/ScooterApp')
+const { describe, expect, test, it } = require("@jest/globals");
 
-// const scooterApp = new ScooterApp()
+const User = require("../src/User");
+const ScooterApp = require("../src/ScooterApp");
+
 // ScooterApp tests here
 
 // register user
-// describe('registerUser method tests', () => {
-//   test('Should return instance of User', () => {
-//     const response = scooterApp.registerUser('Joe Bloggs', 'test123', 21)
-//     expect(response).toBeInstanceOf(User)
-//   })
-// })
+describe("registerUser method tests", () => {
+  it("should return instance of User", () => {
+    const scooterApp = new ScooterApp();
+    const response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
+    expect(response).toBeInstanceOf(User);
+  });
+
+  it("should throw an error if the user is younger than 18", () => {
+    const scooterApp = new ScooterApp();
+    
+    function tryToLogYoungerUser() {
+      scooterApp.registerUser("Anna Tas", "test123", 16);
+    }
+    expect(tryToLogYoungerUser).toThrow("This user is too young to register.");
+  });
+
+  it("should throw an error if the user is already registered", () => {
+    const scooterApp = new ScooterApp();
+    const response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
+
+    function tryToLogRegisteredUser() {
+      scooterApp.registerUser("Joe Bloggs", "test123", 21);
+    }
+
+    expect(tryToLogRegisteredUser).toThrow("This user is already registered.");
+  });
+});
 
 // log in
 
